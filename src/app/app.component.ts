32de07e7a2fe4b84a709b0f6d6a31fbb3e40a0c2
Fabian Freytag <F.Freytag@usu.de>
2018-03-18 23:05:53 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular Showcase';
+  twoWayGettersAndSettersInternal = 'automatic two way data binding';
+  manualTwoWayGettersAndSettersInternal = 'manual two way data binding';
+  propertyBindingInternal = 'property binding';
+
+  get twoWayGettersAndSetters(): string {
+    console.log('Getter for twoWayGettersAndSetters called. Value is [' + this.twoWayGettersAndSettersInternal + ']');
+    return this.twoWayGettersAndSettersInternal;
+  }
+
+  set twoWayGettersAndSetters(value: string) {
+    console.log('Setter for twoWayGettersAndSetters called. new value is [' + value + '], old value was [' + this.twoWayGettersAndSettersInternal + ']');
+    this.twoWayGettersAndSettersInternal = value;
+  }
+
+  get manualTwoWayGettersAndSetters(): string {
+    console.log('Getter for manualTwoWayGettersAndSetters called. Value is [' + this.manualTwoWayGettersAndSettersInternal + ']');
+    return this.manualTwoWayGettersAndSettersInternal;
+  }
+
+  set manualTwoWayGettersAndSetters(value: string) {
+    console.log('Setter for manualTwoWayGettersAndSetters called. new value is [' + value + '], old value was [' + this.manualTwoWayGettersAndSettersInternal + ']');
+    this.manualTwoWayGettersAndSettersInternal = value;
+  }
+
+  propertyBindingRead(): string {
+    console.log('propertyBindingRead called. Value is [' + this.propertyBindingInternal + ']');
+    return this.propertyBindingInternal;
+  }
+
+  propertyBindingWrite(event: any) {
+    console.log('propertyBindingWrite called. New value is [' + event.target.value + '], old value was [' + this.propertyBindingInternal + ']');
+    console.log(event);
+    this.propertyBindingInternal = event.target.value;
+  }
 }
